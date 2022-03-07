04b2d965f15f48a726103ab20a6985a88fbeebcf
JustinCheung168 <jrc17@rice.edu>
2022-03-06 22:32:16 -0600

    big finger-tracker.py update to track both hands, prevent early crashes by writing missing data, change output naming, misc refactoring

--- finger-tracker.py
+++ finger-tracker.py
@@ -1,72 +1,115 @@
-# finger tracking
+# This file tracks the x,y,z coordinates of 10 fingers at once,
+# and records them to a csv file in the output_logs directory.
+# Edit the video_file_name variable to change which video's finger locations are tracked.
+# You may also choose whether or not to see the video as it is being analyzed with display_annotated_video.
+# If you do so, you may exit the video playback early by pressing the Q key.
+
 import cv2
 import mediapipe as mp
-import pickle
 import numpy as np
-import pandas as pd
 import csv
-import os
+from datetime import datetime
+from time import time
 mp_drawing = mp.solutions.drawing_utils # Drawing helpers
-mp_holistic = mp.solutions.holistic # Mediapipe Solutions
+#mp_holistic = mp.solutions.holistic # Mediapipe Solutions - unused
 mp_hands = mp.solutions.hands
 
-# INPUTS
+###
+# INPUTS:
 
 # Define the path to the input video file here:
-# video_file_name = './test_images/5.mp4'
+#video_file_name = './test_images/3-6-2022-1500.mov'
 video_file_name = './test_images/IMG_1286.MOV'
+#video_file_name = './test_images/DisappearingLandmarksTest.MOV'
+#video_file_name = './test_images/DisappearingVerticalFingersTest.MOV'
 
-# Define the desired path to the output csv file containing finger coordinates here:
-csv_output_name = './coords.csv'
+
+# Choose whether or not to see video as it is being analyzed:
+display_annotated_video = True
 
 ###
 
+#to-do tasks:
+
+# 1. see if using "MULTI_HAND_WORLD_LANDMARKS" instead of "MULTI_HAND_LANDMARKS" gives better performance
+# 2. fix inconsistent handedness detection issue
+# 3. fix issue where missing landmarks of hands are somehow still defined when they disappear from frame
 
 
-# This changes the output csv file name to a name that isn't already in use
-file_no=0
-while os.path.isfile(csv_output_name[:-4]+"_"+str(file_no)+".csv"):
-    file_no+=1
-csv_output_name = csv_output_name[:-4]+"_"+str(file_no)+".csv"
 
+
+
+
+t = time() #Set a timer for later to see how long code takes to run
+
+# Define the desired path to the output csv file containing finger coordinates.
+# The output csv file's name indicates when the data was extracted from the video 
+# (NOT WHEN THE VIDEO WAS TAKEN!!!)
+date_time = str(datetime.now().date()) + "_" + str(datetime.now().time())
+csv_output_name = "./output_logs/coords_"+date_time+".csv"
+
+#Generate names of column headers for csv
+landmarks = ['FrameNumber', 'HandsPresent']
+for side in ["Left","Right"]:
+    for fgr in ["Thumb", "Index", "Middle", "Ring", "Pinky"]:
+        for dim in ["X","Y","Z"]:
+            landmarks += [side+fgr+dim]
+
+#Initialize the csv
+with open(csv_output_name, mode='w', newline='') as f:
+    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(landmarks)
+
+#Start reading the video from the file
 cap = cv2.VideoCapture(video_file_name)
-# Initiate holistic model
+frame_no = 0
 with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
     while cap.isOpened():
         ret, frame = cap.read()
 
-        # BGR 2 RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if frame is None:
+            print("Finished analyzing video")
+            break
 
-        # Flip on horizontal
-        image = cv2.flip(image, 1)
-
-        # Set flag
+        # Preprocess image for hands detection
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # BGR 2 RGB (does it matter?)
+        image = cv2.flip(image, 1) # Flip on horizontal since hands face down
         image.flags.writeable = False
 
-        # Detections
+        # Perform detection of all 21 landmarks (fingers, joints, etc.) of a hand.
         results = hands.process(image)
 
-        # Set flag to true
+        # Revert image after hands detection
         image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # RGB 2 BGR
 
-        # RGB 2 BGR
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Detections
+        # pose is a list whose ith entry is the data for the ith hand detected 
+        # (ideally two hands are detected if two hands are in the video...)
         pose = results.multi_hand_landmarks
-        # Rendering results
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                Thumb = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.THUMB_TIP]
-                Index = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-                Middle = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
-                Ring = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
-                Pinky = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.PINKY_TIP]
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
-                                          )
+        # handedness_results is a list whose ith entry is handedness (left or right)
+        # of the ith hand.
+        handedness_results = results.multi_handedness
+
+        # Reformat data for writing into csv
+        if pose: #(if pose exists/is not None)
+            row_partials = {}
+            for hand_idx, hand in enumerate(pose):
+                #Within pose, hand_idx is the index of which hand is detected (should be 0 or 1...)
+                #and hand is the coordinate data for all 21 possible joints in the hand
+                #For more info about mp_hands:
+                #https://google.github.io/mediapipe/solutions/hands.html#multi_hand_landmarks
+
+                handedness = handedness_results[hand_idx].classification[0].label
+
+                #Retrieve fingertip coordinates
+                Thumb = hand.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                Index =  hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                Middle = hand.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                Ring = hand.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
+                Pinky = hand.landmark[mp_hands.HandLandmark.PINKY_TIP]
+
+                #Reformat fingertip coordinates as lists
                 T = list(
                     np.array([[Thumb.x, Thumb.y, Thumb.z] for landmark in pose]).flatten())
                 I = list(
@@ -78,43 +121,52 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
                 P = list(
                     np.array([[Pinky.x, Pinky.y, Pinky.z] for landmark in pose]).flatten())
 
-                    # Concate rows
-                row = T+I+M+R+P
-                class_name = 'Video1'
-                    # Append class name
-                row.insert(0, class_name)
-                num_coords = len(pose)
-                landmarks = ['Frame']
-                for val in range(1, num_coords + 1):
-                    landmarks += ['ThumbX{}'.format(val),'ThumbY{}'.format(val),'ThumbZ{}'.format(val),
-                                  'IndexX{}'.format(val), 'IndexY{}'.format(val),'IndexZ{}'.format(val),
-                                  'MiddleX{}'.format(val), 'MiddleY{}'.format(val),'MiddleZ{}'.format(val),
-                                  'RingX{}'.format(val),'RingY{}'.format(val),'RingZ{}'.format(val),
-                                  'PinkyX{}'.format(val),'PinkyY{}'.format(val),'PinkyZ{}'.format(val)]
-                    # print(landmarks)
-                """
-                this is going to write the landmarks"['class', 'x1', 'y1', 'z1', 'v1', 'x2', 'y2', 'z2', 'v2', 'x3', 'y3', 'z3', 'v3', 'x4', 'y4', 'z4', 'v4', 'x5', 'y5', 'z5', 'v5', 'x6', 'y6', 'z6', 'v6', 'x7', 'y7"
-                to the csv(excel) named "coords" in the same folder
-                use this next line only to create new csv file.it will only create one line of data,
-                 idealy: the step will be uncomment this line, 
-                 commnent the next append csv line and then the file will be cleared.
-                """
+                # Concatenate rows
+                row_partials[handedness] = T+I+M+R+P
 
-                #If the coordinates file doesn't already exist
-                if not os.path.isfile(csv_output_name):
-                    with open(csv_output_name, mode='w', newline='') as f:
-                        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                        csv_writer.writerow(landmarks)
+                if display_annotated_video:
+                    #Draw the skeleton with dots on the joints for this hand
+                    mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                                mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                                mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                                )
 
-                # # #Export to CSV
-                with open(csv_output_name, mode='a', newline='') as f:
-                    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                    csv_writer.writerow(row)
+            #Fill in any missing handed data with None.
+            if ("Left" in row_partials) and ("Right" in row_partials):
+                present_hands = "Both"
+            # If one of the two hands isn't detected, then make all of its finger location entries be None.
+            elif ("Left" not in row_partials) and ("Right" in row_partials):
+                present_hands = "Right"
+                row_partials["Left"] = [None] * 15 #for 5 fingers * 3 x,y,z coords to be rendered null
+            elif ("Left" in row_partials) and ("Right" not in row_partials):
+                present_hands = "Left"
+                row_partials["Right"] = [None] * 15
+            else:
+                #The no-hands case should only occur if pose was None (i.e. outside this if condition);
+                #leaving this here in case that assumption is violated
+                print("NOHANDS ERROR")
+            
+            #Generate row to write into csv
+            row = [frame_no, present_hands] + row_partials["Left"] + row_partials["Right"]
 
-        cv2.imshow('Raw Webcam Feed', image)
+        else: #If no hands were detected (pose is None), just write 30 Nones (2 hands * 5 fingers * 3 x,y,z, coords) to the data.
+            row = [frame_no, "NoHands"] + [None] * 30
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+
+        #Write this frame's data to the csv
+        with open(csv_output_name, mode='a', newline='') as f:
+            csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(row)
+
+
+        frame_no += 1
+
+        if display_annotated_video:
+            cv2.imshow('Video from ' + video_file_name, cv2.flip(image, 1))
+            if cv2.waitKey(10) & 0xFF == ord('q'): #Exit the video playback by pressing the Q key.
+                break
+
+print("Analysis concluded in " + str((time() - t)) + " seconds. Results recorded in " + csv_output_name)
 
 cap.release()
 cv2.destroyAllWindows()
